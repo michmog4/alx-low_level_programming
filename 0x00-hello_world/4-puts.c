957b612a92ccdf3ddffc0b481ee90f9aac8060cf
root <michmog4@gmail.com>
2022-03-10 05:10:16 -0800

    djurjf

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,8 +1,10 @@
-:x
- *  * main - Entry point
- *   *
- *    * Return: Always 0 (Success)
- *     */
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ *Return: Always 0 (Success)
+ */
 int main(void)
 {
 		puts("\"Programming is like building a multilingual puzzle");
