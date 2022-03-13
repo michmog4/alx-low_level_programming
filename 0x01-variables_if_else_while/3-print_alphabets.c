f10e7ed6ce80413232c0f74c6c3a4b0c04374b08
root <michmog4@gmail.com>
2022-03-13 05:27:39 -0700

    3-print_alphabets.c

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -13,8 +13,8 @@ int main(void)
 	for (c = 'a'; c <= 'z'; c++)
 		putchar(c);
 	{
-		for (c = 'C'; c <= 'Z'; c++)
-			putchar(c);	
+		for (c = 'A'; c <= 'Z'; c++)
+		putchar(c);	
 	}
 	putchar('\n');
 		return (0);
