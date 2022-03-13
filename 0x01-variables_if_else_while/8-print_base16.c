3cd762d30f706c69d03c6c8818a1fef238033aa4
root <michmog4@gmail.com>
2022-03-13 09:52:33 -0700

    8-print_base16.c

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -10,12 +10,12 @@ int main(void)
 	 int a = '0';
 	 char c = 'a';
 
-	 for (a = '0'; a <= '9'; a++)
-		 putchar(a);
-	 {
-		 for (c = 'a'; c <= 'f'; c++)
-			 putchar(c);
-	 }
-	 putchar('\n');
-	 return (0);
+	for (a = '0'; a <= '9'; a++)
+		putchar(a);
+	{
+		for (c = 'a'; c <= 'f'; c++)
+			putchar(c);
+	}
+	putchar('\n');
+	return (0);
 }
