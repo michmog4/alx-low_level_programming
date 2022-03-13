ea84622d7438f9677c88186bf17ee80ebeb11986
root <michmog4@gmail.com>
2022-03-13 11:24:15 -0700

    dhdud

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,28 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/*
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int a = '0';
+	int b = '9';
+
+	while (a <= b)
+	{
+		putchar(a);
+	while (a != '9')
+	{
+	putchar(',');
+	putchar(' ');
+	goto s;
+}
+s:
+a++;
+}
+putchar('\n');
+return (0);
+}
