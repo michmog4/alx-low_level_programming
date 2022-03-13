1e23a47e0b8659bcdf74d232d8cd01d9ebb33516
root <michmog4@gmail.com>
2022-03-13 05:12:43 -0700

    3-print_alphabets.c

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+#include <stdlib.h>
+#include <time.h>
+int main(void)
+{
+	char c;
+	for (c = 'C'; c <= 'Z'; c++)
+		putchar(c);
+	{
+		for (c = 'C'; c <= 'Z'; c++)
+			putchar(c);
+	}
+	return(0);
+}
