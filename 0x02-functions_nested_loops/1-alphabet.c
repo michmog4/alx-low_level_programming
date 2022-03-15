0b824099e7d08be96183cf787d6c9e80a0024092
root <michmog4@gmail.com>
2022-03-15 12:07:51 -0700

    cjchc

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,13 +1,13 @@
-#include <unistd.h>
-
+#include "main.h"
 /**
- * _putchar - writes the character c to stdout
- * @c: The character to print
+ * print_alphabet - prints alphabet in lowercase
  *
- * Return: On success 1.
- * On error, -1 is returned, and errno is set appropriately.
- */
-int _putchar(char c)
+ **/
+void print_alphabet(void)
 {
-	return (write(1, &c, 1));
+	char c;
+
+	for (c = 'a'; c <= 'z'; c++)
+		_putchar(c);
+	_putchar('\n');
 }
